a691ad8caac45e2c3c1125dd249358f38910a9fb
Diendorfer <DBALDN@euro.dreibanken.at>
2021-11-27 17:28:51 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGeHiNY7aP0MAG4epOq_-kDCwc3LJVtBk",
+    authDomain: "fir-9-dojo-7f3bc.firebaseapp.com",
+    projectId: "fir-9-dojo-7f3bc",
+    storageBucket: "fir-9-dojo-7f3bc.appspot.com",
+    messagingSenderId: "381987900069",
+    appId: "1:381987900069:web:d9c3121b19383fc9f65b4c"
+};
+
+initializeApp(firebaseConfig)
